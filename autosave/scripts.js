5c7a9cdf1a71d22da93bb19966a76f9d10b7a388
DannyBoyNYC <daniel.deverell@gmail.com>
2018-11-01 21:17:43 -0400

    done

--- autosave/scripts.js
+++ autosave/scripts.js
@@ -1,1 +1,62 @@
-// your scripts go here
+;(function (window, document, undefined) {
+	// Your code goes here...
+})(window, document);
+
+
+;(function (window, document, undefined) {
+  const formElem = document.getElementById('save-me');
+  var log = console.log;
+  var faves = []; // NEW 
+  
+  var saveData = function(){
+    var id = formElem.id;
+    var formData = localStorage.getItem('formData-' + id);
+    formData = formData ? JSON.parse(formData) : {};
+    
+    if (event.target.type === 'checkbox') {
+      formData[event.target.name] = event.target.checked;
+    } else {
+      formData[event.target.name] = event.target.value;
+    }
+		localStorage.setItem('formData-' + id, JSON.stringify(formData));
+  }
+  
+  var getData = function () {
+    var formData = localStorage.getItem('formData-' + formElem.id);
+    formData = JSON.parse(formData);
+    
+    for (var data in formData) {
+      var field = formElem.querySelector('[name="' + data + '"]');
+      
+      if (field.type === 'checkbox') {
+        field.checked = formData[data];
+      }
+      
+      else if (field.type === 'radio') { // NEW
+        var radios = Array.from(formElem.querySelectorAll('input[type="radio"]'));
+        radios.forEach(function (radio) {
+          if (radio.value === formData[data]) {
+            radio.checked = true;
+          }
+        })
+      }
+      else {
+        field.value = formData[data];
+      }
+      log(formData)
+    }
+  }
+  
+  
+  // Reset formData to empty object
+  var resetData = function (event) {
+    var id = formElem.id;
+    localStorage.setItem('formData-' + id, JSON.stringify({}));
+  };
+  
+  // Listen for submit event
+  document.addEventListener('submit', resetData);
+  formElem.addEventListener('input', saveData);
+  window.addEventListener('load', getData);
+  
+})(window, document);
